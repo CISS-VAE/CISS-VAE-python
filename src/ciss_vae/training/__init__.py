b93d3098c118f2118c0f6bb5e23824ddafc2e64d
Dani V <vaithid1@omega00.mskcc.org>
2025-09-16 16:18:39 -0400

    hopefully fixes autotune import in tests

--- src/ciss_vae/training/__init__.py
+++ src/ciss_vae/training/__init__.py
@@ -1,11 +1,11 @@
 from .train_initial import train_vae_initial
 from .train_refit import train_vae_refit
-from .autotune import autotune, SearchSpace
+from .autotune import autotune as run_autotune, SearchSpace
 
 
 
 __all__ = [
     "train_vae_initial",
     "train_vae_refit",
-    'autotune', 'SearchSpace'
+    'run_autotune', 'SearchSpace'
 ]
